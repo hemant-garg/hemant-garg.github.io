d02f88373e2a88a4880b1f5039635f39f345c1df
hemant-garg <hemant.gr6@gmail.com>
2019-03-13 13:15:51 +0530

    resume updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,40 +1,51 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-import MyPhoto from '../MyPhoto/MyPhoto';
-import Button from '../Button/Button';
-import LinkList from '../LinkList/LinkList';
-import Logo from './hemant garg.jpg';
+import { Link } from "react-router-dom";
+import MyPhoto from "../MyPhoto/MyPhoto";
+import Button from "../Button/Button";
+import LinkList from "../LinkList/LinkList";
+import Logo from "./hemant garg.jpg";
 
-
-
-import './Home.css';
+import "./Home.css";
 class Home extends React.Component {
+  goToResume() {
+    window.open(
+      "https://drive.google.com/file/d/1PAOnEvstpYvVID3WjKXgNvxtlGTB5GWW/view?usp=sharing"
+    );
+  }
 
-	
-	goToResume(){
-		window.open('https://drive.google.com/file/d/1FWLhce6EL50awH7SZ093-UzY2uNnG0rj/view');
-	}
-
-	render(){
-		return(
-			<main className="Home">
-				
-				<MyPhoto logo={Logo} width={"27rem"} height={'27rem'}/>
-				<h1 className="my-name" style={{animationDelay: 1.5 + 's'}} >Hemant Garg</h1>
-				<h3 className="my-tagline" style={{animationDelay: 2 + 's'}}>A Full Stack Web Developer who plays Guitar </h3>
-				<div style={{display:'flex', justifyContent: 'center'}}>
-					<Link to="/projects">
-						<Button delay="2.5" color="#6D214F" text="PROJECTS" iconClass = "fas fa-briefcase" />
-					</Link>
-					<Button delay="2.8" onclick={this.goToResume} color="#b71540" text="RESUME" iconClass = "fas fa-location-arrow" />
-				</div>	
-				<div className="social-link-container">
-					<LinkList />
-				</div>
-				
-			</main>
-		)
-	}
-};
+  render() {
+    return (
+      <main className="Home">
+        <MyPhoto logo={Logo} width={"27rem"} height={"27rem"} />
+        <h1 className="my-name" style={{ animationDelay: 1.5 + "s" }}>
+          Hemant Garg
+        </h1>
+        <h3 className="my-tagline" style={{ animationDelay: 2 + "s" }}>
+          A Full Stack Web Developer who plays Guitar{" "}
+        </h3>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Link to="/projects">
+            <Button
+              delay="2.5"
+              color="#6D214F"
+              text="PROJECTS"
+              iconClass="fas fa-briefcase"
+            />
+          </Link>
+          <Button
+            delay="2.8"
+            onclick={this.goToResume}
+            color="#b71540"
+            text="RESUME"
+            iconClass="fas fa-location-arrow"
+          />
+        </div>
+        <div className="social-link-container">
+          <LinkList />
+        </div>
+      </main>
+    );
+  }
+}
 
 export default Home;
