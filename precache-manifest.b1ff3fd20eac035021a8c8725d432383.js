14c466bf0bc58f3008fa8f029699e1d466aa1d21
Hemant <hemant.gr6@gmail.com>
2019-05-31 23:36:15 +0530

    Updates

---
+++ precache-manifest.b1ff3fd20eac035021a8c8725d432383.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7b68ef85304e87fa7971884c2606915",
+    "url": "/static/media/faceScanner.b7b68ef8.png"
+  },
+  {
+    "revision": "e9819e241f57d8531008",
+    "url": "/static/css/main.10e6e0cc.chunk.css"
+  },
+  {
+    "revision": "b3a2cc622a89ab640591",
+    "url": "/static/js/1.b3a2cc62.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9359a36bdbbe316a232d1dbf6510cd7",
+    "url": "/static/media/hemant garg.e9359a36.jpg"
+  },
+  {
+    "revision": "b41112fcb2dc51725e5dd73b61daaacc",
+    "url": "/static/media/back.b41112fc.png"
+  },
+  {
+    "revision": "5ee11082e9751de19128f2ba2778f7f6",
+    "url": "/static/media/ecellmsit.5ee11082.png"
+  },
+  {
+    "revision": "e9819e241f57d8531008",
+    "url": "/static/js/main.e9819e24.chunk.js"
+  },
+  {
+    "revision": "28b9b658bf8687c190aeb101a0b9df02",
+    "url": "/static/media/flatUiColors.28b9b658.png"
+  },
+  {
+    "revision": "e5f4cae4ba6a540715e11f9fe769df4d",
+    "url": "/static/media/mailzy.e5f4cae4.png"
+  },
+  {
+    "revision": "26d83c3024445028da93c77fbc964de6",
+    "url": "/static/media/portfolio.26d83c30.png"
+  },
+  {
+    "revision": "87934c0f7b8eaff63e077391b188de92",
+    "url": "/static/media/socialNetwork.87934c0f.png"
+  },
+  {
+    "revision": "df988a89432f33ef66162bf14cfa2d98",
+    "url": "/static/media/youtubeMini.df988a89.png"
+  },
+  {
+    "revision": "5bf5ff5b99def02fef45929bee8a9fea",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad95da902bb4b89e9aefba4776952079.js"
+  "/precache-manifest.b1ff3fd20eac035021a8c8725d432383.js"
 );
 
 workbox.clientsClaim();
